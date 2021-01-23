f6592770cfd96eb6651f8306824c5ca8facfbb32
Norman <normanramadhan3@gmail.com>
2021-01-23 07:44:29 +0700

    Rearrange global scope

--- src/script.js
+++ src/script.js
@@ -26,6 +26,8 @@ const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
  */
 const fontLoader = new THREE.FontLoader()
 
+const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+
 fontLoader.load(
   '/fonts/helvetiker_regular.typeface.json',
   (font) => {
@@ -43,52 +45,44 @@ fontLoader.load(
         bevelSegments: 4
       }
     )
-    // textGeometry.computeBoundingBox()
-    // textGeometry.translate(
-    //   - (textGeometry.boundingBox.max.x - 0.02) * 0.5, // Subtract bevel size
-    //   - (textGeometry.boundingBox.max.y - 0.02) * 0.5, // Subtract bevel size
-    //   - (textGeometry.boundingBox.max.z - 0.03) * 0.5  // Subtract bevel thickness
-    // )
     textGeometry.center()
-
-    const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
     const text = new THREE.Mesh(textGeometry, material)
     scene.add(text)
-
-    const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45)
-    const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1)
-    
-
-    for (let i = 0; i < 50; i++) {
-      const donut = new THREE.Mesh(donutGeometry, material)
-      donut.position.x = (Math.random() - 0.5) * 10
-      donut.position.y = (Math.random() - 0.5) * 10
-      donut.position.z = (Math.random() - 0.5) * 10
-
-      donut.rotation.x = Math.random() * Math.PI
-      donut.rotation.y = Math.random() * Math.PI
-
-      const scale = Math.random()
-      donut.scale.set(scale, scale, scale)
-
-      scene.add(donut)
-    }
-    for (let i = 0; i < 50; i++) {
-      const box = new THREE.Mesh(boxGeometry, material)
-      box.position.x = (Math.random() - 0.5) * 10
-      box.position.y = (Math.random() - 0.5) * 10
-      box.position.z = (Math.random() - 0.5) * 10
-
-      box.rotation.x = Math.random() * Math.PI
-      box.rotation.y = Math.random() * Math.PI
-
-      const scale = Math.random()
-      box.scale.set(scale, scale, scale)
-
-      scene.add(box)
-    }
   }
 )
+
+const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45)
+const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1)
+
+
+for (let i = 0; i < 50; i++) {
+  const donut = new THREE.Mesh(donutGeometry, material)
+  donut.position.x = (Math.random() - 0.5) * 10
+  donut.position.y = (Math.random() - 0.5) * 10
+  donut.position.z = (Math.random() - 0.5) * 10
+
+  donut.rotation.x = Math.random() * Math.PI
+  donut.rotation.y = Math.random() * Math.PI
+
+  const scale = Math.random()
+  donut.scale.set(scale, scale, scale)
+
+  scene.add(donut)
+}
+for (let i = 0; i < 50; i++) {
+  const box = new THREE.Mesh(boxGeometry, material)
+  box.position.x = (Math.random() - 0.5) * 10
+  box.position.y = (Math.random() - 0.5) * 10
+  box.position.z = (Math.random() - 0.5) * 10
+
+  box.rotation.x = Math.random() * Math.PI
+  box.rotation.y = Math.random() * Math.PI
+
+  const scale = Math.random()
+  box.scale.set(scale, scale, scale)
+
+  scene.add(box)
+}
 
 /**
  * Sizes
